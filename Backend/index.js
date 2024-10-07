5b6b5c2e9c59cc4b4033fbf2db6af4d3e47c22c5
Aadarsh Desai <adidesai91@gmail.com>
2024-10-07 00:58:14 -0400

    Added zod validation to index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -4,16 +4,35 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/todo", function(req, res){
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){  // Early return
+        res.status(411).json({
+            message: "You sent the wrong inputs"
+        });
+        return;
+    }
+
+    // Put data in mongoDB
+});
+
 app.get("/todos", function(req, res){
 
 });
 
-app.post("/todo", createTodo, function(req, res){
 
-});
+app.put("/completed", function(req, res){
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            message: "You sent wrong inputs"
+        });
+        return;
+    }
 
-app.put("/completed", updateTodo, function(req, res){
-
+    // Update database
 });
 
 app.listen(3000);
