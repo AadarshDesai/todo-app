9502deb5c38ba1349198b4229cbd79fa717ae2b9
Aadarsh Desai <adidesai91@gmail.com>
2024-10-10 21:05:28 -0400

    useEffect hook is added to fetch the todos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,12 +7,14 @@ import { Todos } from '../Components/Todos'
 
 function App() {
   const [todos, setTodos] = useState([]);
-
-  fetch("http://localhost:3000/todos")
+  
+  useEffect(function(){
+    fetch("http://localhost:3000/todos")
     .then(async function(res){
       const json = await res.json();
       setTodos(json.todos);
     });
+  }, []);
 
   return (
     <div>
